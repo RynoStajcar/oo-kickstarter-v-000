b9bed8a025557db4c07efbf8441dcaf4a8d2c8fa
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-24 16:41:35 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,8 +8,8 @@ class Project
     @backers = []
   end
 
-  def add_backer(name)
-    backers << name
+  def add_backer(backer)
+    backers << backer
     backer.backed_projects << self
   end
 end
