2eb9e15ca97fe3531ab48491c0fd446221116973
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-24 16:14:52 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,15 @@
 class Backer
 
-  attr_accessor :name
-
+  attr_accessor :name, :backed_projects
+  
   def initialize(name)
     @name = name
+    @backed_projects = []
   end
 
+  def back_project(title)
+   @backed_projects << Project.new(title)
+  end
+  
+    
 end
